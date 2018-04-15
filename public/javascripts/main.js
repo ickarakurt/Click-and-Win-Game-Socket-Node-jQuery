e433f86867984025b31f21a34a6ee49de4752af8
ickarakurt <ickarakurt@gmail.com>
2018-04-15 21:45:42 +0300

    selam

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -40,7 +40,7 @@ $(() => {
 
 
 
-    const socket = io.connect('http://localhost:3000', {
+    const socket = io.connect('https://click-and-win.herokuapp.com:80', {
         reconnectionAttempts: 4,
         reconnectionDelay: 3000,
         // reconnection: false
